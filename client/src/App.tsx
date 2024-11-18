a4c0dd477a7171958bdfff6098725bc13bb2eb8c
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-11-18 19:38:02 +0200

    Implement WebRTC connection management in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,12 +17,14 @@ import useSocketConnection from './hooks/useSocketConnection'
 
 import type User from '@/types/User.type'
 
+import createWebRtcConnection from './utils/createWebRtcConnection'
+
 function App() {
    const [roomID, setRoomID] = useLocalStorage('last-room-id', '')
    const [name, setName] = useLocalStorage('client-name', '')
    const [submitted, setSubmitted] = useState(false)
    const [loading, setLoading] = useState(false)
-   const [users, setUsers] = useState<User[]>([])
+   let [users, setUsers] = useState<User[]>([])
 
    const { socket, error } = useSocketConnection({
       setLoading,
@@ -34,21 +36,145 @@ function App() {
    root.classList.remove('light', 'dark')
    root.classList.add('dark')
 
+   const updateUsers = (newUsers: User[]) => {
+      setUsers(newUsers)
+      users = newUsers
+   }
+
    useEffect(() => {
       if (!socket) return
       socket.emit('join-room', roomID)
 
-      socket.on('room-users', (users) => {
+      socket.on('room-users', (usr) => {
          setLoading(false)
-         setUsers(users)
+         updateUsers(usr)
+
+         usr.forEach((user: User) => {
+            if (user.id === socket.id) return
+
+            const peer = createWebRtcConnection({
+               onSignal: (signal) => {
+                  console.log('[CALLING]', user.id)
+                  socket.emit('call', {
+                     user: user.id,
+                     signal,
+                  })
+               },
+               onConnect: () => {
+                  console.log('[CONNECTED]', user.id)
+                  peer.send('Hello')
+
+                  updateUsers(
+                     users.map((u) => {
+                        if (u.id === user.id) {
+                           return { ...u, isConnected: true }
+                        }
+                        return u
+                     })
+                  )
+               },
+               onClose: () => {
+                  console.log('[CLOSED]', user.id)
+
+                  updateUsers(
+                     users.map((u) => {
+                        if (u.id === user.id) {
+                           return { ...u, peer: undefined, isConnected: false }
+                        }
+                        return u
+                     })
+                  )
+               },
+               onData: (data) => {
+                  console.log('Data received', data)
+               },
+            })
+
+            updateUsers(
+               users.map((u) => {
+                  if (u.id === user.id) {
+                     return { ...u, peer }
+                  }
+                  return u
+               })
+            )
+         })
+      })
+
+      socket.on('call', ({ caller, signal }) => {
+         const user = users.find((u) => u.id === caller)
+         if (!user) return
+         console.log('[ANSWERING]', caller)
+
+         const peer = createWebRtcConnection({
+            initiator: false,
+            onSignal: (signal) => {
+               console.log('[ACCEPTING]', caller)
+               socket.emit('answer', {
+                  user: caller,
+                  signal,
+               })
+            },
+            onConnect: () => {
+               console.log('[CONNECTED]', caller)
+               setUsers((prev) =>
+                  prev.map((u) => {
+                     if (u.id === caller) {
+                        return { ...u, isConnected: true }
+                     }
+                     return u
+                  })
+               )
+            },
+            onClose: () => {
+               console.log('[CLOSED]', caller)
+               setUsers((prev) =>
+                  prev.map((u) => {
+                     if (u.id === caller) {
+                        return { ...u, peer: undefined, isConnected: false }
+                     }
+                     return u
+                  })
+               )
+            },
+            onData: (data) => {
+               console.log('Data received', data)
+            },
+         })
+
+         peer.signal(signal)
+
+         updateUsers(
+            users.map((u) => {
+               if (u.id === caller) {
+                  return { ...u, peer }
+               }
+               return u
+            })
+         )
+      })
+
+      socket.on('answer', ({ answerID, signal }) => {
+         const user = users.find((u) => u.id === answerID)
+         if (!user) return
+         console.log('[ANSWERED]', answerID)
+         console.log(user.peer?.signal)
+         user.peer?.signal(signal)
       })
 
       socket.on('user-connected', (user) => {
-         setUsers((prev) => [...prev, user])
+         console.log('User connected', user)
+         updateUsers([...users, user])
       })
 
       socket.on('user-disconnected', (user) => {
-         setUsers((prev) => prev.filter((u) => u.id !== user.id))
+         console.log('User disconnected', user)
+         users.forEach((u) => {
+            if (u.id === user.id && u.peer) {
+               console.log('Closing peer connection', u?.peer?.destroy?.())
+            }
+         })
+         updateUsers(users.filter((u) => u.id !== user.id))
       })
 
       return () => {
