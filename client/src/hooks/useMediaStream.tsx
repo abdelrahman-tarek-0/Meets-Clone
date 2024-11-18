2ab241e3be2441c08d9fa84bf29d87400a3c12bb
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-11-18 22:07:01 +0200

    Refactor useMediaStream hook to manage media stream cleanup and handle undefined state

--- client/src/hooks/useMediaStream.tsx
+++ client/src/hooks/useMediaStream.tsx
@@ -1,14 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { toast } from 'sonner'
 
 export default function useMediaStream(constraint: MediaStreamConstraints) {
-   const [stream, setStream] = useState<MediaStream | null>(null)
+   const [stream, setStream] = useState<MediaStream | undefined>(undefined)
+   const streamRef = useRef<MediaStream | null>(null) // To hold the latest stream
 
    useEffect(() => {
+      if(!constraint.audio && !constraint.video) return
+      console.log('Requesting media stream')
       navigator.mediaDevices
          .getUserMedia(constraint)
          .then((stream) => {
             setStream(stream)
+            streamRef.current = stream
          })
          .catch((error) => {
             toast.error('Failed to access media devices')
@@ -16,11 +20,15 @@ export default function useMediaStream(constraint: MediaStreamConstraints) {
          })
 
       return () => {
-         stream?.getTracks().forEach((track) => {
+         console.log('Cleaning up media stream')
+         streamRef.current?.getTracks().forEach((track) => {
+            console.log('Stopping track', track)
             track.stop()
          })
+         setStream(undefined)
+         streamRef.current = null
       }
-   }, [])
+   }, [constraint])
 
    return stream
 }
