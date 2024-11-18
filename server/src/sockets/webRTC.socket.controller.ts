a4ec2156c6c3abd94f650026b053f672ab2da21b
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-11-18 22:06:27 +0200

    Refactor WebRTC socket controller to use 'signal' event for signaling and remove deprecated 'answer' event

--- server/src/sockets/webRTC.socket.controller.ts
+++ server/src/sockets/webRTC.socket.controller.ts
@@ -8,10 +8,17 @@ export default (socket: Socket, io: Server) => {
       })
    })
 
-   socket.on('answer', (data) => {
-      io.to(data?.user).emit('answer', {
-         answerID: socket.id,
+   socket.on('signal', (data) => {
+      io.to(data?.user).emit('signal', {
          signal: data.signal,
+         caller: socket.id,
       })
    })
+
+   // socket.on('answer', (data) => {
+   //    io.to(data?.user).emit('answer', {
+   //       answerID: socket.id,
+   //       signal: data.signal,
+   //    })
+   // })
 }
