b93ae344a8bfd19e14f433abc23accd01397d732
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-11-18 22:04:57 +0200

    Enhance LoginCard component with audio and video stream controls

--- client/src/components/LoginCard.tsx
+++ client/src/components/LoginCard.tsx
@@ -9,8 +9,10 @@ import {
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { Checkbox } from '@/components/ui/checkbox'
 
 import LiveLogo from '/images/WhiteLiveLogo.gif?url'
+import { Camera, CameraOff, Mic, MicOff } from 'lucide-react'
 
 interface LoginCardProps {
    name: string
@@ -18,6 +20,8 @@ interface LoginCardProps {
    roomID: string
    setRoomID: (roomID: string) => void
    setSubmitted: (submitted: boolean) => void
+   streamConstraints: MediaStreamConstraints
+   setStreamConstraints: (constraints: MediaStreamConstraints) => void
 }
 
 export default function LoginCard({
@@ -26,6 +30,8 @@ export default function LoginCard({
    roomID,
    setRoomID,
    setSubmitted,
+   streamConstraints,
+   setStreamConstraints,
 }: LoginCardProps) {
    return (
       <Card className="w-[350px]">
@@ -72,13 +78,72 @@ export default function LoginCard({
                      />
                   </div>
                   <div className="flex flex-col space-y-1.5">
-                     <Label htmlFor="framework">Room Id</Label>
+                     <Label htmlFor="roomID">Room Id</Label>
                      <Input
-                        id="framework"
+                        id="roomID"
                         placeholder="Enter room id"
                         value={roomID}
                         onChange={(e) => setRoomID(e.target.value)}
                      />
+                  </div>
+
+                  <div 
+                     className="flex space-x-4" 
+                     style={{ 
+                        justifyContent: 'center',
+                        marginTop: '1rem',
+                     }}
+                  >
+                  <div className="flex items-center justify-left">
+                     {streamConstraints.audio ? (
+                        <label htmlFor="audio" className="cursor-pointer">
+                           <Mic />
+                        </label>
+                     ) : (
+                        <label htmlFor="audio" className="cursor-pointer">
+                           <MicOff />
+                        </label>
+                     )}
+                     <Checkbox
+                        checked={streamConstraints.audio ? true : false}
+                        style={{
+                           visibility: 'hidden',
+                        }}
+                        id="audio"
+                        onCheckedChange={(e) => {
+                           console.log(e.valueOf())
+                           setStreamConstraints({
+                              ...streamConstraints,
+                              audio: e.valueOf() as boolean,
+                           })
+                        }}
+                     />
+                  </div>
+                  <div className="flex items-center justify-left">
+                     {streamConstraints.video ? (
+                        <label htmlFor="video" className="cursor-pointer">
+                           <Camera />
+                        </label>
+                     ) : (
+                        <label htmlFor="video" className="cursor-pointer">
+                           <CameraOff />
+                        </label>
+                     )}
+                     <Checkbox
+                        checked={streamConstraints.video ? true : false}
+                        style={{
+                           visibility: 'hidden',
+                        }}
+                        id="video"
+                        onCheckedChange={(e) => {
+                           console.log(e.valueOf())
+                           setStreamConstraints({
+                              ...streamConstraints,
+                              video: e.valueOf() as boolean,
+                           })
+                        }}
+                     />
+                  </div>
                   </div>
                </div>
             </form>
