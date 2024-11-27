29a13eb0f1e5897b804217cec54162399c2988b6
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-11-27 02:06:27 +0200

    refactor: comment out unused connection cleanup logic in Room component

--- client/src/components/Room.tsx
+++ client/src/components/Room.tsx
@@ -63,12 +63,6 @@ export default function Room({
             <CardFooter>
                <Button
                   onClick={() => {
-                     // users.map((user) => {
-                     //    Object.values(user?.connections || {}).forEach((conn) => {
-                     //       if(conn.destroyed) return
-                     //       conn?.destroy?.()
-                     //    })
-                     // })
                      setSubmitted(false)
                   }}
                >
