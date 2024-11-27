153abf6b53fe58280f885f4cfdeddbcb9c373709
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-11-27 02:07:03 +0200

    refactor: remove unused connections property from User type definition

--- client/src/types/User.type.ts
+++ client/src/types/User.type.ts
@@ -1,11 +1,7 @@
-import type { ExtendedPeer } from '@/utils/webRTC.helpers'
 
 type User = {
    id: string
    name: string
-   connections?: {
-      [key: string]: ExtendedPeer
-   }
    isConnected?: boolean
    stream?: MediaStream
 }
