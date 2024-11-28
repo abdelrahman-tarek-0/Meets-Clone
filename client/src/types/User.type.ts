f971ada71bde15c51b49a5519641ca452f392a0e
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-11-28 03:58:43 +0200

    feat: add message property to User type for handling chat messages

--- client/src/types/User.type.ts
+++ client/src/types/User.type.ts
@@ -1,9 +1,12 @@
-
 type User = {
    id: string
    name: string
    isConnected?: boolean
    stream?: MediaStream
+   message?: {
+      text?: string
+      type?: string
+   }
 }
 
 export default User
